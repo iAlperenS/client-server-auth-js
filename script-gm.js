aacc5c7d0f203cce4a3765a3e77a9e551b201f7f
Alperen <130500193+iAlperenS@users.noreply.github.com>
2024-11-11 12:59:47 +0300

    Create script-gm.js

---
+++ script-gm.js
@@ -0,0 +1,42 @@
+// You need GM_xmlhttpRequest to use this
+
+let glx = GM_xmlhttpRequest;
+function ch() {
+      return new Promise((resolve, reject) => {
+          glx({
+              method: "GET",
+              url: "http://localhost:5000/get_hwid_status",
+              onload: function(rp) {
+                  if (rp.status === 200) {
+                      const rs = JSON.parse(rp.responseText);
+                      resolve(rs.status === "valid");
+                  } else {
+                      hx()
+                      re()
+                  }
+              },
+              onerror: function() {
+                hx()
+                re()
+              }
+          });
+      });
+  }
+
+  function hx() {
+      document.body.style.display = "none";  }
+
+  function re() {
+      window.location.href = "https://github.com/iAlperenS";
+   }
+
+  ch().then(isValid => {
+      if (isValid) {
+      } else {
+          hx()
+          re()
+      }
+  }).catch(error => {
+      hx()
+      re()
+  });
